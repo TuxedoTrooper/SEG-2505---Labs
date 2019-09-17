594b521a6372d2979e7a8e3e7a1ffb16e19cee04
Cristopher McIntyre Garcia <25cristopher25@gmail.com>
2019-09-17 18:16:15 -0400

    Cris commit

--- PersonInfo1.java
+++ PersonInfo1.java
@@ -1,8 +1,8 @@
 public class PersonInfo1 {
 
-    public static String firstName = "Leisha";
-    public static String lastName = "Baker";
-    public static int studentNo = 338299;
+    public static String firstName = "Cristopher";
+    public static String lastName = "McIntyre Garcia";
+    public static int studentNo = 300025114;
     public static String description = "Likes to take pictures";
 
     public static void displayFr() {
