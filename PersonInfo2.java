a3b859204e8a118adf87491e4aa69584f65f14bd
Gabriel <gfour085@uottawa.ca>
2019-09-17 17:48:38 -0400

    First commit

---
+++ PersonInfo2.java
@@ -0,0 +1,28 @@
+public class PersonInfo2 {
+
+    public static String firstName = "Gabriel";
+    public static String lastName = "Fournier";
+    public static int studentNo = 8731580;
+    public static String description = "PcMasterRace";
+
+    public static void displayFr() {
+        String out = SIConstants.GREETING_FR;
+        out += " " + firstName + " " + lastName + '.';
+        out += SIConstants.STUDENT_NUMBER_FR + studentNo + '.';
+        out += SIConstants.DESCRIPTION_FR + description + '.';
+        out += SIConstants.EOL;
+        
+        System.out.println(out);
+    }
+
+    
+    public static void displayEn() {
+        String out = SIConstants.GREETING_EN;
+        out += " " + firstName + " " + lastName + '.';
+        out += SIConstants.STUDENT_NUMBER_EN + studentNo + '.';
+        out += SIConstants.DESCRIPTION_EN + description + '.';
+        out += SIConstants.EOL;
+        
+        System.out.println(out);
+    }
+}
